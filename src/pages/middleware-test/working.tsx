af98dad3e7c2a17a73a441e2a98399bb55fae060
Diana Secrieru <secrieru_diana@yahoo.com>
2023-01-31 14:28:59 +0000

    Update url

--- src/pages/middleware-test/working.tsx
+++ src/pages/middleware-test/working.tsx
@@ -12,7 +12,7 @@ const MiddlewareWorkingPage = ({ data }: any) => {
 };
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const res = await fetch(`${process.env.SITE_URL}/api/getDate`);
+  const res = await fetch('https://main.d2ewcijckzgu5l.amplifyapp.com/api/getDate');
   const data = await res.json();
 
   return { props: { data } };
